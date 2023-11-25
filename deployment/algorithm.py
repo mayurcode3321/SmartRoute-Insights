68c9942460d3dc49574fddcf7f40a90ee5809821
Mayur <mp624859@gmail.com>
2023-11-25 09:45:55 +0530

    Add comment line

--- deployment/algorithm.py
+++ deployment/algorithm.py
@@ -1,4 +1,5 @@
-# limit the number of cpus used by high performance libraries
+# limit the number of cpus used by high performance libraries4
+# set environment path 
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
